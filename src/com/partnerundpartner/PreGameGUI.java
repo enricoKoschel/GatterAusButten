b86910e94775de329eded770ea395d31291e6d2a
enricoKoschel <enricokoschel02@gmail.com>
2021-03-07 22:03:16 +0100

    Implement settings GUI

--- src/com/partnerundpartner/PreGameGUI.java
+++ src/com/partnerundpartner/PreGameGUI.java
@@ -1,12 +1,36 @@
 package com.partnerundpartner;
 
 import javax.swing.*;
+import java.util.HashMap;
 
 public class PreGameGUI extends JFrame {
 	private JPanel mainPanel;
 	private JLabel lblTitle;
+
 	private JLabel lblDifficulty;
-	private JComboBox cbxDifficulty;
+	private JComboBox<String> cbxDifficulty;
+
+	private JLabel lblSize;
+	private JSpinner spnPlayFieldSize;
+
+	private JLabel lblAgain;
+	private JCheckBox chkAgain;
+
+	private JLabel lblForceRatio;
+	private JCheckBox chkForceRatio;
+
+	private JLabel lblWindowSize;
+	private JSpinner spnWindowSize;
+
+	private JLabel lbl3Long;
+	private JSpinner spn3Long;
+
+	private JLabel lbl2Long;
+	private JSpinner spn2Long;
+
+	private JLabel lbl1Long;
+	private JSpinner spn1Long;
+
 	private JButton btnStartGame;
 
 	public boolean gameStarted;
@@ -14,12 +38,6 @@ public class PreGameGUI extends JFrame {
 
 	public PreGameGUI(String title) {
 		super(title);
-
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
 
 		setContentPane(mainPanel);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -38,5 +56,59 @@ public class PreGameGUI extends JFrame {
 			setVisible(false);
 			dispose();
 		});
+	}
+
+	private void createUIComponents() {
+		spnPlayFieldSize = new JSpinner(new SpinnerNumberModel(7, 1, 26, 1));
+
+		spnWindowSize = new JSpinner(new SpinnerNumberModel(1280, 250, null, 1));
+
+		spn3Long = new JSpinner(new SpinnerNumberModel(2, 0, null, 1));
+		spn2Long = new JSpinner(new SpinnerNumberModel(2, 0, null, 1));
+		spn1Long = new JSpinner(new SpinnerNumberModel(3, 0, null, 1));
+
+		cbxDifficulty = new JComboBox<>(new String[]{"Einfach", "Normal", "Schwer", "Unmöglich"});
+		cbxDifficulty.setSelectedIndex(2);
+	}
+
+	public AI.Difficulty getSelectedDifficulty() {
+		switch (cbxDifficulty.getSelectedIndex()) {
+			case 0:
+				return AI.Difficulty.Easy;
+			case 1:
+				return AI.Difficulty.Medium;
+			case 2:
+				return AI.Difficulty.Hard;
+			case 3:
+				return AI.Difficulty.Impossible;
+		}
+
+		throw new IllegalStateException("Invalid difficulty selected!");
+	}
+
+	public int getSelectedPlayFieldSize() {
+		return (int)spnPlayFieldSize.getValue();
+	}
+
+	public boolean getSelectedTurnOrder() {
+		return chkAgain.isSelected();
+	}
+
+	public boolean getSelectedForceRatio() {
+		return chkForceRatio.isSelected();
+	}
+
+	public int getSelectedWindowSize() {
+		return (int)spnWindowSize.getValue();
+	}
+
+	public HashMap<Integer, Integer> getSelectedShipAmounts() {
+		HashMap<Integer, Integer> shipAmounts = new HashMap<>();
+
+		shipAmounts.put(1, (int)spn1Long.getValue());
+		shipAmounts.put(2, (int)spn2Long.getValue());
+		shipAmounts.put(3, (int)spn3Long.getValue());
+
+		return shipAmounts;
 	}
 }
